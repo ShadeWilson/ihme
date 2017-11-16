99010d3f12ba7e4e3366812a223ad4e586a8e58a
Shade Wilson <shadew@ihme.washington.edu>
2017-11-16 15:52:35 -0800

    Initial commit

---
+++ setup.R
@@ -0,0 +1,23 @@
+# Shade Wilson
+# 11/16/2017
+# Automated set up of j and h roots plus username
+# can specify desired variable names, deults given
+
+
+setup <- function(j_root_name = "j_root", h_root_name = "h_root", user_name = "user") {
+  
+  user <- Sys.info()[["user"]]
+  
+  if (Sys.info()["sysname"] == "Linux") {
+    j_root <- "/home/j/"
+    h_root <- "~/"
+  } else { 
+    j_root <- "J:/"
+    h_root <- "H:/"
+  }
+  
+  # assign root values to global variables (or else they're lost in scope!)
+  assign(j_root_name, j_root, envir = .GlobalEnv)
+  assign(h_root_name, h_root, envir = .GlobalEnv)
+  assign(user_name, user, envir = .GlobalEnv)
+}
