ebaa26a2e5c918518e0478d6ac1c33c9bc45fd77
Shade Wilson <shadew@ihme.washington.edu>
2017-12-07 12:08:12 -0800

    Add vars for repo url and name; add deletion of temp file

--- R/clone_filepath.R
+++ R/clone_filepath.R
@@ -1,32 +1,34 @@
 # do the git cloning here
 # git clone git@github.com:whatever folder-name
 
-# IDEA: check if exist, if not then clone and use :)
+# wouldnt be have to grep for repo name
 
-git_clone <- function(repo) {
-  # check if the repo has already been cloned
-  if ("ihme_r_pkg_files" %in% list.files(paste0(h_root))) {
-    inner_path <- paste0(h_root, "ihme_r_pkg_files/inner_path")
-    return(readChar(inner_path, file.info(inner_path)$size))
+git_clone <- function(repo_url, repo_name = NULL) {
+
+  if (is.null(repo_name)) {
+    intermediate_name <- stringr::str_extract(repo_url, "[^/]*.git$")
+    repo_name <- stringr::str_extract(intermediate_name, "^[^.]*")
   }
+
+  # check if the repo has already been cloned
+  # if (repo_name %in% list.files(paste0(h_root))) {
+  #   inner_path <- paste0(h_root, repo_name, "/inner_path")
+  #   return(readChar(inner_path, file.info(inner_path)$size))
+  # }
 
   # if not, check the OS. Matters because linux/mac commands WONT work on windows
   if (Sys.info()["sysname"] %in% c("Linux", "Darwin")) {
-    system(paste0("git clone", repo))
+    system(paste0("git clone", repo_url, " ", h_root, repo_name))
   } else {
     # create temp shell script to run on Windows command line that opens a bash shell and executes
     temp_file <- "git_clone.sh"
     shell(paste0("ECHO #!/bin/bash > ", h_root, temp_file))
-    shell(paste0("ECHO git clone ", repo, " >> ", h_root, temp_file))
+    shell(paste0("ECHO git clone ", repo_url , " ", h_root, repo_name, " >> ", h_root, temp_file))
     shell(paste0(h_root, temp_file))
-    #shell(paste0("del ", h_root, temp_file)) # delete temp shell script
+    shell(paste0("del /f H:\\", temp_file)) # delete temp shell script
   }
-
-  inner_path <- paste0(h_root, "ihme_r_pkg_files/inner_path")
-  return(readChar(inner_path, file.info(inner_path)$size))
 }
 
-git_clone("https://shadew@stash.ihme.washington.edu/scm/~shadew/ihme_r_pkg_files.git")
-
+#git_clone("https://shadew@stash.ihme.washington.edu/scm/~shadew/ihme_r_pkg_files.git")
 
 
