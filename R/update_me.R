1f7a7b21eeee37ffb174ca295679504f8d638a60
Shade Wilson <shadew@ihme.washington.edu>
2017-12-16 15:29:28 -0800

    Adapt package version checking and create wrapper for multiple

--- R/update_me.R
+++ R/update_me.R
@@ -3,6 +3,8 @@
 # Can we figure out a good way to get this r package onto peoples computer for use on the cluster?
 # it appears not, but we can automate the process or at least give people an alternative interface to do it?
 
+# Adapted some ideas from http://thecoatlessprofessor.com/programming/automatically-check-if-r-package-is-the-latest-version-on-package-load/
+
 library(magrittr)
 
 
@@ -11,6 +13,8 @@ update_me_at <- function(folder) {
   args <- paste0('--library=\"', folder, "\"")
 
   devtools::install_github("ShadeWilson/ihme", args = args, force = TRUE)
+
+  message("Please refresh your session of R to use the updated version of package ihme.")
 
 }
 
@@ -28,46 +32,44 @@ available[available$Package %in% packages, ] %>% View()
 
 utils::packageDescription('ihme')
 
-pkgVersionCRAN = function(pkg, cran_url="http://cran.r-project.org/web/packages/")
-{
+package_version_cran = function(package, cran_url="http://cran.r-project.org/web/packages/") {
 
   # Create URL
-  cran_pkg_loc = paste0(cran_url,pkg)
+  cran_pkg_loc <- paste0(cran_url, package)
 
   # Try to establish a connection
-  suppressWarnings( conn <- try( url(cran_pkg_loc) , silent=TRUE ) )
+  suppressWarnings(conn <- try(url(cran_pkg_loc), silent = TRUE))
 
   # If connection, try to parse values, otherwise return NULL
-  if ( all( class(conn) != "try-error") ) {
-    suppressWarnings( cran_pkg_page <- try( readLines(conn) , silent=TRUE ) )
+  if (all(class(conn) != "try-error")) {
+    suppressWarnings(cran_pkg_page <- try(readLines(conn), silent = TRUE))
     close(conn)
   } else {
     return(NULL)
   }
 
   # Extract version info
-  version_line = cran_pkg_page[grep("Version:",cran_pkg_page)+1]
+  version_line <- cran_pkg_page[grep("Version:", cran_pkg_page) + 1]
   gsub("<(td|\\/td)>","",version_line)
 }
 
-pkgVersionCRAN("ggplot2")
+package_version_cran("ggplot2")
 
 
-check_package <- function(...){
-  # Avoid running if in batch job / user not present
-  if (!interactive()) return()
+check_package <- function(package, folder){
+  message(paste0(package, ":"))
 
   # Obtain the installed package information
-  local_version = utils::packageDescription('ggplot2', lib.loc = folder)$Version
+  local_package <- utils::packageDescription(package, lib.loc = folder)
 
   # Grab the package information from CRAN
-  cran_version = pkgVersionCRAN("ggplot2")
+  cran_version <- package_version_cran(package)
 
   # Verify we have package information
   if(!is.null(cran_version) && length(cran_version) != 0L){
-    latest_version = utils::compareVersion(cran_version, local_version)
+    latest_version = utils::compareVersion(cran_version, local_package$Version)
 
-    d = if(latest_version == 0){
+    status <- if(latest_version == 0){
       'CURRENT'
     }else if(latest_version == 1){
       'OUT OF DATE'
@@ -76,15 +78,28 @@ check_package <- function(...){
     }
 
   }else{ # Gracefully fail.
-    d = "ERROR IN OBTAINING REMOTE VERSION INFO"
-    latest_version = 0
+    status <- "ERROR IN OBTAINING REMOTE VERSION INFO"
+    latest_version <- 0
   }
 
-  # Use packageStartUpMessages() so that folks can suppress package messages with
-  # suppressPackageStartupMessages(library(pkg))
-  packageStartupMessage('Version: ', local_version$Version, ' (', d,') built on ', local_version$Date)
+  message(paste0('Version: ', local_package$Version, ' (', status,') of ', package, ' built on ', local_package$Date))
+
   if(latest_version == 1){
-    packageStartupMessage('\n!!! NEW VERSION ', cran_version , ' !!!')
-    packageStartupMessage('Download the latest version: ',cran_version,' from CRAN via `install.packages("pkgname")`\n')
+    message(paste0("Package ", package, " out of date. Update as soon as possible."))
   }
 }
+
+
+check_package(package = "ggplot2", folder = folder)
+
+
+# wrapper for check_package, allows easy way to check multiple packages in a single folder
+check_package_all <- function(packages, folder) {
+  invisible(lapply(packages, check_package, folder = folder))
+}
+
+invisible(lapply(packages, check_package, folder = folder))
+
+
+
+
