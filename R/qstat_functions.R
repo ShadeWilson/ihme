1a4ff6ff0774084ea0a322dee9bdae5415117095
Shade Wilson <shadew@ihme.washington.edu>
2018-02-05 17:31:22 -0800

    Write up qstat basics

---
+++ R/qstat_functions.R
@@ -0,0 +1,44 @@
+# Author: Shade Wilson
+# 2/5/18
+# qstating through R
+# making complicated qstat outputs easier to comprend by leveraging the better interface of R
+# (will need to be on the cluster to use, obvi)
+
+qstat <- function(print = FALSE) {
+  object <- system("qstat -u emgold2", intern = TRUE)
+
+  if (print) {
+    system("qstat")
+  }
+  return(object)
+}
+qstat()
+
+trim <- function(string) {
+  gsub("(^ )|( $)", "", string)
+}
+
+split_whitespace <- function(string) {
+  strsplit(string, "[ \t]+")
+}
+
+cat <- qstat()
+cat1 <- trim(cat)
+
+split <- split_whitespace(cat1)
+
+rows <- rbind(split[[3]], split[[4]])
+df <- as.data.frame(rows)
+setNames(df, split[[1]])
+
+
+strsplit(cat1, " +")
+
+
+
+
+
+
+
+
+
