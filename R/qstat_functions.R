0732af44aab3062bff49069b22b032cdc2ff5476
Shade Wilson <shadew@uw.edu>
2018-08-02 15:28:50 -0700

    Commit dismod status function

--- R/qstat_functions.R
+++ R/qstat_functions.R
@@ -4,40 +4,6 @@
 # making complicated qstat outputs easier to comprend by leveraging the better interface of R
 # (will need to be on the cluster to use, obvi)
 
-qstat <- function(print = FALSE) {
-  object <- system("qstat", intern = TRUE)
-
-  if (print) {
-    system("qstat")
-  }
-  return(object)
-}
-q <- qstat()
-q <- as.data.frame(q)
-
-#
-# trim <- function(string) {
-#   gsub("(^ )|( $)", "", string)
-# }
-#
-# split_whitespace <- function(string) {
-#   strsplit(string, "[ \t]+")
-# }
-#
-# cat <- qstat()
-# cat1 <- trim(cat)
-#
-# split <- split_whitespace(cat1)
-#
-# rows <- rbind(split[[3]], split[[4]])
-# df <- as.data.frame(rows)
-# setNames(df, split[[1]])
-#
-#
-# strsplit(cat1, " +")
-#
-#
-#
 
 #' @author Shade Wilson
 
@@ -48,29 +14,38 @@ library(dplyr)
 
 setup()
 source_functions(get_location_metadata = T)
+source()
 
 loc_data <- get_location_metadata(35)
 
-#'Returns the number of jobs running at each location level
+#' Returns the number of jobs running at each location level
 #'
 #' @param MVID dismod model version ID
+#' @param user uuw net id of modeler running DisMod
 #' @param location_metadata df returned from get_location_metadata shared function
 #'
-dismod_status <- function(MVID, location_metadata = loc_data) {
-  qstat <- system("qstat -xml", intern = TRUE)
+dismod_status <- function(MVID, user, location_metadata = loc_data) {
+  qstat <- system(paste0("qstat -u ", user, " -xml"), intern = TRUE)
 
   qstat_list <- xmlToList(qstat)
   job_info <- as.list(qstat_list[["job_info"]])
+  queue_info <- as.list(qstat_list[["queue_info"]])
 
   df <- data.frame(matrix(unlist(job_info), nrow=length(job_info), byrow=T))
+  queue_df <- data.frame(matrix(unlist(queue_info), nrow=length(queue_info), byrow=T)) %>%
+    select(-X7, X7 = X8, X8 = X9)
+
+
+  df <- rbind(df, queue_df)
+
   names(df) <- c("JB_job_number", "JAT_prio", "JB_name", "JB_owner", "state_id", "JB_submission_time", "slots", "state")
 
   df <- df %>%
+    filter(grepl("dm_", JB_name)) %>%
     mutate(mvid = regmatches(JB_name, regexpr("^(dm_\\d{6})", JB_name)),
                   mvid = gsub("dm_", "", mvid),
                   job_name_detail = gsub("^(dm_\\d{6})_", "", JB_name),
                   location_id = gsub("_[mf]_\\d+_.*$", "", job_name_detail))
-
 
   dm <- filter(df, mvid == MVID)
 
@@ -81,14 +56,37 @@ dismod_status <- function(MVID, location_metadata = loc_data) {
   dm <- left_join(dm, location_metadata, by = "location_id") %>%
     mutate(location_type = if_else(is.na(location_type), location_id, location_type))
 
-  job_count <- group_by(dm, location_type, state_id) %>% count()
+  levels <- c("G0", "superregion", "region", "admin0", "varnish")
+
+  job_count <- group_by(dm, mvid, location_name, location_type, state_id) %>%
+    summarize(jobs_left = n()) %>%
+    mutate(progress = if_else(!is.na(location_name), paste0(signif((1 - jobs_left / 12) * 100, digits = 2), "%"), ""))
+
+  # order the jobs from global to subnational
+  job_count$location_type <- factor(job_count$location_type, levels = levels)
+  job_count <- arrange(job_count, location_type)
+
   return(job_count)
 }
 
 # example
-dismod_status(MVID = 327020)
+hmwe_mvids <- c(329123, 329837, 329852)
+
+dismod_status(MVID = 329123, user = "hmwekyu")
 
 
+
+
+
+# as ----------------------------------------------------------------------
+
+library(rjson)
+json <- fromJSON(file = paste0(h_root, "private_url.json"))
+
+
+post_slack <- function(message, url) {
+  system(paste0("curl -X POST -H \'Content-type: application/json' --data \'{\"text\": \"", message, "\", \"link_names\": 1}\' ", url))
+}
 
 
 
